3070c80190c7f05fb4953c485a52aec94db7337d
nefretac <karahanyavuz61@gmail.com>
2024-04-28 18:08:47 +0300

    creating the project and making the db connection

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    private String url = "jdbc:mysql://localhost/airline management system";
+    private String user = "user";
+    private String password = "airline61";
+
+    public static void main(String[]args){
+
+    }
+    private Connection connection(){
+
+    }
+}
---
+++ src/Passenger.java
@@ -0,0 +1,44 @@
+public class Passenger {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String Tel;
+    private String email;
+
+    public Passenger(){}
+       public void setId( int id) {
+            this.id = id;
+        }
+        public int getId (){
+           return id;
+        }
+        public void setFirstName(String firstName) {
+        this.firstName = firstName;
+        }
+        public String getFirstName() {
+        return firstName;
+        }
+
+        public void setLastName(String lastName){
+        this.lastName = lastName;
+        }
+        public String getLastName(){
+        return lastName;
+        }
+
+        public void setTel(String Tel){
+        this.Tel = Tel;
+        }
+        public String getTel(){
+        return Tel;
+        }
+
+        public void setEmail(String email){
+        this.email = email;
+        }
+
+        public String getEmail(){
+        return email;
+        }
+    }
